f2690d3ba5e6fe24548544b7b87808b6388bc66d
Thiago Hirata <hirata@devcase.com.br>
2015-07-20 17:13:57 -0300

    Versão Líbero

---
+++ include/footer.php
@@ -0,0 +1,54 @@
+<footer id="back-rodape">
+	<div id="box30">
+    	<div id="box31">
+        	<div id="english">
+            	<select name="idioma" class=" select">
+                <option value="English">English </option>
+                <option value="Spanish">Spanish </option>
+                <option value="Portuguese">Portuguese </option>
+                </select>
+                
+                <select name="dinheiro" class=" select">
+                <option value="Dolar">Dolar </option>
+                <option value="Euro">Euro </option>
+                <option value="Real">Real </option>
+                </select>
+            </div>
+            
+            <div id="company">
+           	  		<h2>Company</h2>
+                    <a href="index.php">Home </a><br />
+              		<a href="about.php">About </a><br>
+                    <a href="recomendacoes.php">Tips</a><br>
+					<a href="#">Jobs</a><br>
+  					<a href="#">Policies</a><br>
+  					<a href="#">Terms & Privacy</a>  		
+            </div>
+            
+            <div id="discover">
+            	<h2>Discover</h2>
+            	<p>How It Works<br>Invite Friends<br>Site Map</p>
+            </div>
+            
+            <div id="downlo-adapp">
+           	  <h2>Download our free mobile app</h2>
+           	  <div id="box-img1"><img src="images/app1.jpg"></div>
+              <div id="box-img2"><img src="images/app2.jpg"></div>
+              <p>Copyright 2015 - TravenUp - All rights reserved</p>
+          </div>
+   
+        </div>
+        
+    	<div id="join-us-on">
+			<h2>Join Us On</h2>
+            <div id="box-join-us-on">
+            	<div id="box-face"><img src="images/fd-face.fw.png"></div>
+                <div id="box-twi"><img src="images/fd-twi.png"></div>
+            	<div id="box-in"><img src="images/fd-in.png"></div>
+                <div id="box-ista"><img src="images/fd-in.png"></div>
+                <div id="box-goog"><img src="images/fd-goog.png"></div>
+            </div>
+            	        
+        </div>
+    </div><!-- fim box30 -->
+</footer>
---
+++ include/header.php
@@ -0,0 +1,33 @@
+<section class="topo">
+	<a href="index.php"><img src="images/universal-joias-logo.png"></a>
+    
+    <div class="fb-like" style="position:absolute; margin-top:20px; left:0; z-index:999999; margin-left:20px; position:fixed;" data-href="https://www.facebook.com/pages/Universal-J%C3%B3ias/1425736011003588?fref=ts" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+    
+</section>
+
+<nav class="menu">
+	<ul>
+		<li style="margin-left:31px;"><a href="index.php">Home</a></li>
+         <li class="ico-menu">•</li>
+        <li ><a href="joalheria-sao-jose-dos-campos.php">Sobre a Universal</a></li>
+        <li class="ico-menu">•</li>
+		<li><a href="joalheria-vale-paraiba.php">Jóias</a></li>
+        <li class="ico-menu">•</li>
+		<li><a href="relojoaria-sao-jose-campos.php">Relógios</a></li>
+        <li class="ico-menu">•</li>
+        <li><a href="moda-vale-paraiba.php">Beleza</a></li>
+        <li class="ico-menu">•</li>
+        <li><a href="lojas-universal-joias.php">Lojas</a></li>
+        <li class="ico-menu">•</li>
+        <li><a href="contato-universal-joias.php">Contato</a></li>
+	</ul>
+</nav>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
